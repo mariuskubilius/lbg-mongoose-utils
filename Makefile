4016fffe02d6150888fd1c0e8922a380adb5a522
Marius Kubilius <marius.kubilius@gmail.com>
2013-07-18 18:00:39 +0300

    adding status module to the index.js

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 test:
 	@./node_modules/.bin/mocha
 
+test-cov: lib-cov
+	@EXPRESS_COV=1 $(MAKE) test REPORTER=html-cov > coverage.html
+
 .PHONY: test
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var slugify = require('./lib/slugify');
 var ancestorTree = require('./lib/ancestorTree');
+var status = require('./lib/status');
 module.exports['slugify'] = slugify;
 module.exports['ancestorTree'] = ancestorTree;
-
+module.exports['status'] = status;
